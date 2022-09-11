c682621a7a385382f54270ec17f71a608a8e548b
Prahadeeshwaran <95561624+prahadeeshwaran-k@users.noreply.github.com>
2022-09-11 09:51:57 +0530

    OOP

---
+++ src/main/kotlin/Inheritance.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>){
+
+}
+open class Dog{
+    var size: Int = 0
+
+    fun bark(){
+        println("Bark")
+    }
+    fun play(){
+        println("Playing")
+    }
+}
+class SubDog:Dog(){
+
+}
