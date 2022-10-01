c665eb870ed9b0632590b4c68ddf79e414c849d4
Prahadeeshwaran <prahadeeshwaran.mk@gmail.com>
2022-10-01 08:30:53 +0530

    list

---
+++ src/main/kotlin/_5_ListAndArrayList/04_List functions.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val colours = listOf<String>("blue","red","green")
+    println(colours.size)
+    println(colours.contains("blur"))
+    println(colours.contains("pink"))
+    val colours2= listOf<String>("red","blue")
+    println(colours.containsAll(colours2))
+}
